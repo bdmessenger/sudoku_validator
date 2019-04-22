82232ee233823fc319634a3fc2b8e738f50150ff
Brant <bran.bd@gmail.com>
2019-04-22 00:56:48 -0500

    update_3

---
+++ grid.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="index.css">
+</head>
+<body>
+	<div class="sudoku">
+		<div class="wrapper">
+			<div class="one">
+				<input type="text" id="cell_input"  name="row_1_slot_1" size="1" maxlength="1" value="5">
+			</div>
+			<div class="two">2</div>
+      <div class="three">3</div>
+      <div class="four">3</div>
+      <div class="five">3</div>
+      <div class="six">3</div>
+      <div class="seven">3</div>
+      <div class="eight">3</div>
+      <div class="nine">3</div>
+<!--       <div class="ten">3</div> -->
+  </div></br></br></br>
+  <div class="wrapper">
+			<div class="one">
+				<input type="text" id="cell_input"  name="row_1_slot_1" size="1" maxlength="1" value="5">
+			</div>
+			<div class="two">2</div>
+      <div class="three">3</div>
+      <div class="four">3</div>
+      <div class="five">3</div>
+      <div class="six">3</div>
+      <div class="seven">3</div>
+      <div class="eight">3</div>
+      <div class="nine">3</div>
+		</div>
+</br></br></br>
+  <div class="wrapper">
+			<div class="one">
+				<input type="text" id="cell_input"  name="row_1_slot_1" size="1" maxlength="1" value="5">
+			</div>
+			<div class="two">2</div>
+      <div class="three">3</div>
+      <div class="four">3</div>
+      <div class="five">3</div>
+      <div class="six">3</div>
+      <div class="seven">3</div>
+      <div class="eight">3</div>
+      <div class="nine">3</div>
+		</div>
+</br></br></br>
+  <div class="wrapper">
+			<div class="one">
+				<input type="text" id="cell_input"  name="row_1_slot_1" size="1" maxlength="1" value="5">
+			</div>
+			<div class="two">2</div>
+      <div class="three">3</div>
+      <div class="four">3</div>
+      <div class="five">3</div>
+      <div class="six">3</div>
+      <div class="seven">3</div>
+      <div class="eight">3</div>
+      <div class="nine">3</div>
+		</div>
+</br></br></br>
+  <div class="wrapper">
+			<div class="one">
+				<input type="text" id="cell_input"  name="row_1_slot_1" size="1" maxlength="1" value="5">
+			</div>
+			<div class="two">2</div>
+      <div class="three">3</div>
+      <div class="four">3</div>
+      <div class="five">3</div>
+      <div class="six">3</div>
+      <div class="seven">3</div>
+      <div class="eight">3</div>
+      <div class="nine">3</div>
+		</div>
+</br></br></br>
+  <div class="wrapper">
+			<div class="one">
+				<input type="text" id="cell_input"  name="row_1_slot_1" size="1" maxlength="1" value="5">
+			</div>
+			<div class="two">2</div>
+      <div class="three">3</div>
+      <div class="four">3</div>
+      <div class="five">3</div>
+      <div class="six">3</div>
+      <div class="seven">3</div>
+      <div class="eight">3</div>
+      <div class="nine">3</div>
+		</div>
+    <div class="vl1"></div>
+    <div class="vl2"></div>
+    <div class="hl1"></div>
+    <div class="hl2"></div>
+<!-- 		<canvas id="sudoku" width="500" height="500" style="border:1px solid #d3d3d3;"></canvas> -->
+	</div>
+	<script src="index.js"></script>
+	<script>sudoku_board();</script>
+</body>
+</html>
